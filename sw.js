627a1e2915a24816b4055c6355570d40ccc4d876
marcelwagner-afk <marcel.wagner@w-dfs.de>
2026-08-04 10:57:52 +0200

    Version 5: Echte italienische Audio-Ausgabe (neuronale Stimme)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "italiano-v4";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "italiano-v5";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./audio-it.mp3", "./audio-sprites.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
